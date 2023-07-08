7fa5a35873750fbdec83720f5e1f3cfb3982e15b
tanu <dastanushri402@gmail.com>
2023-07-08 21:46:53 +0600

    update 404 file

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,7 +12,7 @@ const ErrorPage = () => {
     <div className='flex justify-center items-center h-screen'>
       <div className='text-center'>
         <h2 className='text-3xl mb-5 font-bold text-red-500'>Oops, Sorry!</h2>
-        <h4 className='text-xl font-bold text-red-600 mb-3'>Page Not Found</h4>
+        <h4 className='text-xl font-bold text-red-600 mb-4'>Page Not Found</h4>
         <button
           className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'
           onClick={goHome}
