c4d51743284f8c9bb3f510057a24108dc4a30f78
tanu <dastanushri402@gmail.com>
2023-07-08 09:07:36 +0600

    create responsive navbar

--- components/Shared/Navbar/Navbar.tsx
+++ components/Shared/Navbar/Navbar.tsx
@@ -1,53 +1,80 @@
-"use client";
-import Link from 'next/link'
-import React from 'react';
-import styles from './navbar.module.css'
-
-const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "Contact",
-      url: "/contact",
-    },
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
-  return (
-    <div className={styles.container}>
-        <Link href="/" className={styles.logo}>Lamamia</Link>
-        <div className={styles.links}>
-            {
-                links.map((link) => (<Link key={link.id} href={link.url}  className={styles.link}>{link.title}</Link>))
-            }
-            
-        </div>
-    </div>
-  )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <h3 className="ml-2 text-2xl text-white font-semibold">TeuxDeux</h3>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              onClick={toggleMenu}
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isMenuOpen ? (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div className={`${isMenuOpen ? 'block' : 'hidden'} hidden sm:flex sm:items-center sm:ml-6`}>
+            <div className="ml-4 flex items-center sm:ml-6 sm:space-x-4">
+              <Link href="/" className="block text-white hover:bg-gray-700 text-lg" passHref>
+                Home
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className={`${isMenuOpen ? 'block' : 'hidden'} sm:hidden`}>
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <Link href="/" className="block text-white hover:bg-gray-700 text-lg" passHref>
+              Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
