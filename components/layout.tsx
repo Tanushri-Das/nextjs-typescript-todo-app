50b09da56498d5f446d4d1f6a4ff424ef63934d4
tanu <dastanushri402@gmail.com>
2023-07-08 12:18:34 +0600

    setup 404 page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,17 +1,23 @@
-import React, { ReactNode } from 'react';
-import Navbar from './Shared/Navbar/Navbar';
-import Footer from './Shared/Footer/Footer';
+
+import React, { ReactNode } from "react";
+import Navbar from "./Shared/Navbar/Navbar";
+import Footer from "./Shared/Footer/Footer";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
-  children: ReactNode;
-}
+    children: ReactNode;
+  }
 
-export default function Layout({ children }: LayoutProps) {
-  return (
-    <>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </>
-  );
+const Layout = ({children}: LayoutProps) => {
+    const router = useRouter();
+    
+    return (  
+        <div className="container">
+            {router.pathname !== "/404" && <Navbar />}
+            {children}
+           {router.pathname !== "/404" && <Footer />}
+        </div>
+    );
 }
+ 
+export default Layout;
---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const ErrorPage = () => {
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='text-center'>
+        <h2 className='text-3xl mb-5 font-bold text-red-500'>Oops, Sorry!</h2>
+        <h4 className='text-xl font-bold text-red-600 mb-3'>Page Not Found</h4>
+        <button
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'
+          onClick={goHome}
+        >
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
