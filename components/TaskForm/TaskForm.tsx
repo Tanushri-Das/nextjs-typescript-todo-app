9a56b47284be1cbe7719a6ae965f9a403c70a03f
tanu <dastanushri402@gmail.com>
2023-07-08 22:07:39 +0600

    update error page button

--- components/TaskForm/TaskForm.tsx
+++ components/TaskForm/TaskForm.tsx
@@ -199,7 +199,7 @@ const TaskForm: React.FC<TaskFormProps> = ({
         <div className="flex items-center justify-center">
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Add Task
           </button>
